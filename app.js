ce736df408ff7847ea02521f4256e1fc65d0723f
v4ne55a14 <vanessa.uclm@gmail.com>
2023-10-21 18:15:50 +0200

    Desarrollo de las pruebas del modelo en versión de línea de comandos Class_CarRentalOnline: Creación e inicialización.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("¡Hola Mundo!");
 
 const express = require('express'); const app = express();
-const path = require('path'); app.use('/actividadDOM', express.static(path.join(__dirname, 'public')));
+const path = require('path'); app.use('/car-rental-online-p2', express.static(path.join(__dirname, 'public')));
 const PORT = 3000; app.listen(PORT, ()=>{
     console.log('Ejecutando Servidor en el puerto '+PORT);
 })
